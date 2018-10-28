cada593ba6b52a20ef79848a64522c7eb7b469ba
DuilioFugazzaro <34074352+DuilioFugazzaro@users.noreply.github.com>
2018-10-28 17:40:55 +0100

    Add files via upload

--- functions.php
+++ functions.php
@@ -33,7 +33,28 @@ if(!function_exists('hs_setup_theme')){
 
 }
 
-add_action('after_setup_theme', 'hs_setup_theme');
+/* Register Sidebars
+-------------------------------------------------------- */
+
+if(! function_exists('hs_sidebars') ) {
+
+  function hs_sidebars(){
+    register_sidebar(array(
+      'name' => esc_html__('Primary','hs'),
+      'id' => 'primary',
+      'description' => esc_html__('Main Sidebar','hs'),
+      'before_title' => '<h3>' ,
+      'after_title' => '</h3>',
+      'before_widget' => '<div class="widget  %2$s clearfix">',
+      'after_widget' => '</div>',
+
+      )
+    );
+  }
+
+}
+
+add_action('widgets_init','hs_sidebars');
 
 /* Include Javascript Files
 --------------------------------------------------*/
@@ -74,30 +95,63 @@ function hs_styles(){
 
 add_action('wp_enqueue_scripts' , 'hs_styles');
 
-/***** SHORTCODE *****/
 
-// function PrimoShortcode(){
 
-//   return 'Al momento lo shortcode FUNZIONA';
+// Register Custom Post Type
+function custom_post_type() {
 
-// }
-// add_shortcode( 'primo ', 'PrimoShortcode' );
+	$labels = array(
+		'name'                  => _x( 'Slides', 'Post Type General Name', 'text_domain' ),
+		'singular_name'         => _x( 'Slide', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'             => __( 'Slides', 'text_domain' ),
+		'name_admin_bar'        => __( 'Slides', 'text_domain' ),
+		'archives'              => __( 'Slides Archives', 'text_domain' ),
+		'attributes'            => __( 'Slides Attributes', 'text_domain' ),
+		'parent_item_colon'     => __( 'Parent Slides:', 'text_domain' ),
+		'all_items'             => __( 'All Slides', 'text_domain' ),
+		'add_new_item'          => __( 'Add New Slides', 'text_domain' ),
+		'add_new'               => __( 'Add New Slide', 'text_domain' ),
+		'new_item'              => __( 'New Slide', 'text_domain' ),
+		'edit_item'             => __( 'Edit Slides', 'text_domain' ),
+		'update_item'           => __( 'Update Slide', 'text_domain' ),
+		'view_item'             => __( 'View Slide', 'text_domain' ),
+		'view_items'            => __( 'View Slides', 'text_domain' ),
+		'search_items'          => __( 'Search Slides', 'text_domain' ),
+		'not_found'             => __( 'Slides Not found', 'text_domain' ),
+		'not_found_in_trash'    => __( 'Slides Not found in Trash', 'text_domain' ),
+		'featured_image'        => __( 'Featured Image', 'text_domain' ),
+		'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
+		'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
+		'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
+		'insert_into_item'      => __( 'Insert into Slide', 'text_domain' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this Slides', 'text_domain' ),
+		'items_list'            => __( 'Slides list', 'text_domain' ),
+		'items_list_navigation' => __( 'Slides list navigation', 'text_domain' ),
+		'filter_items_list'     => __( 'Filter Slides list', 'text_domain' ),
+	);
+	$args = array(
+		'label'                 => __( 'Slide', 'text_domain' ),
+		'description'           => __( 'Custom Post Type for Carousel', 'text_domain' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'thumbnail', 'comments', 'trackbacks', 'revisions', 'custom-fields', 'page-attributes', 'post-formats' ),
+		'taxonomies'            => array( 'category', 'post_tag' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 25,
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+	);
+	register_post_type( 'slide', $args );
 
-//[foobar]
-function foobar_func( $atts ){
-	return "foo and bar";
 }
-add_shortcode( 'foobar', 'foobar_func' );
-
-
-/* REDUX FRAMEWORK */
-
-//including Redux Framework option folder
-require 'admin-folder/admin/admin-init.php';
-
-
-
-
+add_action( 'init', 'custom_post_type', 0 );
 
 
 
